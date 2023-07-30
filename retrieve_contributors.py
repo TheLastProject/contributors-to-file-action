25a43f1f1a6582049c4eeac0bacc49f2a360f162
FC Stegerman <flx@obfusk.net>
2023-07-30 14:01:22 +0200

    code cleanup & use $GITHUB_TOKEN if set

--- retrieve_contributors.py
+++ retrieve_contributors.py
@@ -11,20 +11,31 @@ def wait_until_api_allowed():
         print("API limit reached, waiting 5 minutes...")
         time.sleep(300)
 
+
 def get_name(contributor):
+    if contributor["type"] == "Anonymous":
+        return contributor["name"]
+
     if "url" in contributor:
         print(f"Resolving name of {contributor['login']}...")
         wait_until_api_allowed()
-        contributor_data = requests.get(contributor["url"]).json()
+        contributor_data = requests.get(contributor["url"], headers=headers).json()
 
-        if "name" in contributor_data and contributor_data["name"]:
+        if contributor_data.get("name"):
             return contributor_data["name"]
 
         return contributor_data["login"]
 
-    return contributor["name"]
+    return contributor["login"]
+
 
 file_in_repo = sys.argv[1]
+
+token = os.environ.get("GITHUB_TOKEN") or None
+headers = dict(Authorization=f"token {token}") if token else {}
+
+if token:
+    print("Using GitHub token")
 
 contributors = {}
 
@@ -32,16 +43,17 @@ print("Retrieving GitHub contributor list...")
 contributors_url = f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/contributors"
 while True:
     wait_until_api_allowed()
-    r = requests.get(contributors_url, params={'anon': 1})
+    r = requests.get(contributors_url, params={'anon': 1}, headers=headers)
     for contributor in r.json():
         name = get_name(contributor)
-        
+
         if any(bot_string in name for bot_string in ["(bot)", "[bot]"]):
             print(f"Ignoring {name}, matches bot string")
             continue
 
         contributions = contributor["contributions"]
 
+        # might be listed twice, e.g. once Anonymous
         if name in contributors:
             contributions += contributors[name]
 
@@ -49,7 +61,7 @@ while True:
 
         print(f"Set contribution count for {name} to {contributions}")
 
-    if not 'next' in r.links:
+    if 'next' not in r.links:
         break
 
     contributors_url = r.links['next']['url']
