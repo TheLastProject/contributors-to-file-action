0bdf0777971afb48c8627a5ef2049cdb73128fe0
FC Stegerman <flx@obfusk.net>
2023-09-11 01:19:57 +0200

    pass GITHUB_TOKEN via env instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
-token="$1"
-file_in_repo="$2"
+file_in_repo="$1"
 
 mkdir -p "$(dirname "$file_in_repo")"
-python3 /retrieve_contributors.py "$token" "$file_in_repo"
+python3 /retrieve_contributors.py "$file_in_repo"
